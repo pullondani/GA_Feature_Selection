b2673f3c3f5085ab81632ca57857d4b93becbf3f
Daniel Pullon <danielpullon@gmail.com>
2021-09-12 20:00:00 +1200

    Working and with classifier for filter

--- ga-feature-selection.py
+++ ga-feature-selection.py
@@ -1,40 +1,77 @@
+import copy
 from pathlib import Path
 from random import randint, choices, random
 from sklearn.preprocessing import KBinsDiscretizer
 from sklearn.feature_selection import mutual_info_classif
 import csv
-import sys
 from math import log2
 from scipy.stats import entropy
+from datetime import datetime
+import matplotlib.pyplot as plt
+from copy import deepcopy, copy
+from sklearn.naive_bayes import CategoricalNB
 
-NUM_GEN = 100
-# NUM_FEAT = 30
-POP_SIZE = 100
-ELITISM = 0.1
-MUTATION_CHANCE = 0.1
-ALPHA = 0.1
-N_BINS = 30
+
+NUM_GEN = 10
+POP_SIZE = 50
+ELITISM = 0.2
+MUTATION_CHANCE = 0.5
+N_BINS = 10
 
 
 def main():
+    subsets = []
+    
     data, classes, feature_count = read_file('./wbcd.data', './wbcd.names')
-    print(feature_count)
-    run_feature_selection(data, classes, feature_count)
 
+    discretizer = KBinsDiscretizer(
+        n_bins=N_BINS, encode='ordinal', strategy='kmeans')
+    discretizer.fit(data)
+    transformed_data = discretizer.transform(data)
 
-def calc_class_probs(classes):
-    c1 = 0
-    c2 = 0
-    for i in classes:
-        if i == 1:
-            c1 += 1
-        elif i == 2:
-            c2 += 1
+    for i in range(1, 5):
+        start_filter = datetime.now()
+        best_feasible, best_fit, history, best_ent = run_feature_selection(transformed_data, deepcopy(classes), copy(feature_count))
+        end_filter = datetime.now()
+        subsets.append(best_feasible)
+        print('Run', i, 'Time elapsed: ' + str(end_filter - start_filter))
 
-    pc1 = c1 / len(classes)
-    pc2 = c2 / len(classes)
+    # for subset in subsets:
+    subset = subsets[0]
+    num_cols = sum(subset)
+    subset_data = [[0 for x in range(num_cols)] for y in range(len(transformed_data))]
 
-    return [pc1, pc2]
+    subset_col = 0
+    for col, val in enumerate(subset):
+        if val == 1:
+            for row in range(len(transformed_data)):
+                subset_data[row][subset_col] = transformed_data[row][col]
+            subset_col += 1
+
+    test_size = 199 #int(len(subset_data)/2)
+
+    clf = CategoricalNB()
+    clf.fit(subset_data[:-test_size], classes[:-test_size])
+    len(subset_data[-test_size:])
+    knn = clf.predict(subset_data[-test_size:])
+    print(len(knn))
+
+    right = 0
+    test = classes[-test_size:]
+    for i, c in enumerate(knn):
+        if c == test[i]:
+            right += 1
+    
+    print(str((right / test_size * 100)) + '% Correctly Classified')
+    print(best_feasible, best_fit, best_ent)
+
+    plt.plot(list(range(NUM_GEN)), history, label='Mean Fitness')
+    plt.legend()
+    plt.title('Fitness through the generations')
+    plt.xlabel('Generations')
+    plt.ylabel('Fitness')
+    plt.show()
+
 
 
 def run_feature_selection(data, classes, feature_count):
@@ -44,33 +81,43 @@ def run_feature_selection(data, classes, feature_count):
            for ___ in range(POP_SIZE)]
 
     pop_fit = [None] * POP_SIZE
-    best_feasible = None
-    best_fit = 0
+    pop_ent = [None] * POP_SIZE
 
-    discretizer = KBinsDiscretizer(
-        n_bins=N_BINS, encode='ordinal', strategy='kmeans')
-    discretizer.fit(data)
-    transformed_data = discretizer.transform(data)
+    history = []
+    best_feasible = None
+    best_fit = float('-inf')
+
 
     ent = -sum([prob * log2(prob) for prob in class_probs])
 
     for __ in range(NUM_GEN):
+        print('GEN', __)
+        # print(pop[:5])
         for i in range(len(pop)):
-            pop_fit[i] = calc_feature_selection(pop[i], transformed_data, ent, classes)
+            pop_fit[i], pop_ent[i] = calc_mutual_information(pop[i], data, ent, classes)
 
         # Find new best feasible
         best_ind = max(range(len(pop_fit)), key=pop_fit.__getitem__)
+        best_ent = min(pop_ent)
+
+        if pop_ent[best_ind] != best_ent:
+            print(pop_ent[best_ind], best_ent)
+        history.append(pop_ent[best_ind])
+        # print(best_ind, pop_fit[best_ind])
         if pop_fit[best_ind] > best_fit:
             best_feasible = pop[best_ind]
             best_fit = pop_fit[best_ind]
 
+        if pop_ent[best_ind] < best_ent:
+            best_ent = pop_ent[best_ind]
+
         pop = create_new_pop(pop, pop_fit, feature_count)
         pop_fit = [None] * POP_SIZE
 
-    return best_feasible
+    return best_feasible, best_fit, history, best_ent
 
 # Fitness calculation, filter function.
-def calc_feature_selection(individual, data, ent, classes):
+def calc_mutual_information(individual, data, ent, classes):
     # Its not about the rows at all. The individuals are just what features to select. There are 30 features
     rows_count = len(data)
     
@@ -109,15 +156,32 @@ def calc_feature_selection(individual, data, ent, classes):
     total *= -1
 
     # TODO Something isn't right with the total...
-    print(ent, total, ent - total)
-    return ent - total
+    # print(ent - total)
 
+    # YOU WANT LOW ENTROPY!!!!!
+    # BUT YOU WANT HIGH MUTUAL INFORMATION (which is the equation below!)
+    return ent - total, total
+
+
+def calc_class_probs(classes):
+    c1 = 0
+    c2 = 0
+    for i in classes:
+        if i == 1:
+            c1 += 1
+        elif i == 2:
+            c2 += 1
+
+    pc1 = c1 / len(classes)
+    pc2 = c2 / len(classes)
+
+    return [pc1, pc2]
 
 
 # Deep copy arrays and then do elitism.
 def elitism_selection(pop, fits):
-    clone = pop.copy()
-    clone_fit = fits.copy()
+    clone = deepcopy(pop)
+    clone_fit = deepcopy(fits)
     elitists = []
 
     for __ in range(round(ELITISM * POP_SIZE)):
@@ -160,7 +224,7 @@ def mutation_selection(pop, indiv_len):
     for indiv in pop:
         if random() <= MUTATION_CHANCE:
             flip_ind = randint(0, indiv_len-1)
-            print(flip_ind, indiv_len, indiv)
+            # print(flip_ind, indiv_len, indiv)
             indiv[flip_ind] = 0 if indiv[flip_ind] == 1 else 1
 
     return pop
